c0fbe66d7b80256e031cecbcac235407dbbdbb9f
Ian Gibbs <realflash.uk@googlemail.com>
2016-01-12 21:28:00 +0000

    Change names of AWS environment variables to match AWS names to avoid confusion

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+AWS_ID=${AWS_ID}
 AWS_KEY=${AWS_KEY}
-AWS_SECRET=${AWS_SECRET}
 BUCKET=${BUCKET}
 S3FS_OPTS=${S3FS_OPTS}
 
 /etc/init.d/nfs-kernel-server stop
 
-echo "${AWS_KEY}:${AWS_SECRET}" > /etc/passwd-s3fs
+echo "${AWS_ID}:${AWS_KEY}" > /etc/passwd-s3fs
 chmod 0600 /etc/passwd-s3fs
 
 s3fs ${BUCKET} /s3bucket ${S3FS_OPTS}
